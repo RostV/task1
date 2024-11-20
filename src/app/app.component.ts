12bf1c57f06e83d4a571724bd78cb8f889aa4fe0
Rostislav <rost.vasilenko@outlook.com>
2024-11-20 17:35:44 +0300

    task1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +12,21 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'task1';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'Песочница';
+
+  items: (string | number)[] = [1, 2, 3];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.items.push('Новое значение');
+      this.cdr.detectChanges();
+    }, 1000);
+  }
+  ngAfterViewInit(): void {}
 }
